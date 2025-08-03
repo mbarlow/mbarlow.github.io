32c460d8243690836041e46b2c600a1ab71c8269
mbarlow <michaeljbarlow@gmail.com>
2025-08-03 01:29:17 -0400

    cleanup

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,3 +1,2 @@
-export { EntityFactory } from './EntityFactory.js';
-export { SystemPromptBuilder } from './SystemPromptBuilder.js';
-export { IndicatorAnimation, IndicatorAnimationPresets, IndicatorAnimationManager } from './IndicatorAnimations.js';
+export { EntityFactory } from "./EntityFactory.js";
+export { SystemPromptBuilder } from "./SystemPromptBuilder.js";
